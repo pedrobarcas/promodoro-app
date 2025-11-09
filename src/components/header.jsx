2fc49aa364aa401c14b2640d4ef301f313793271
pedrobarcas <pedrobarcas.git@gmail.com>
2025-11-09 20:10:33 -0300

    Adicionado roteamento entre páginas e layouts

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/modules/header.module.css";
+export function Header() {
+  return (
+    <header>
+      <span>Logo</span>
+      <div>
+        <span>Icone</span>
+        <span>Icone</span>
+      </div>
+    </header>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+import "./styles/base.css"
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  </StrictMode>,
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </StrictMode>
 )
